0995c74c6a8da41fc6a7f5658b258a0ce216687e
Zack-Ausmus <zackausmus@gmail.com>
2026-09-03 22:21:31 +0000

    hi

---
+++ cubeface.py
@@ -0,0 +1,10 @@
+cubeface=[0,0,0,0,0,0,0,0,0]
+
+cubletrow=[0,0,0]
+
+cubematrix=[
+        cubletrow,
+        cubletrow,
+        [0,0,0]
+]
+print(cubematrix)
---
+++ newday.py
@@ -0,0 +1,4 @@
+import random
+d6=[1,2,3,4,5,6]
+dice=random.choice(d6)
+print(f"dice: {dice}")
