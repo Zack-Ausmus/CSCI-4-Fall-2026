a315f7bc774b718c69ec005083643c6651028dd9
Zack-Ausmus <zackausmus@gmail.com>
2026-09-19 02:11:35 +0000

    homework2

--- homework2.py
+++ homework2.py
@@ -4,7 +4,7 @@ print("Free Flow Speed Calculator for a Basic Highway Segment Per NCEES 2026")
 #ask user for inputs
 BFFS=(75.4)
 flw=float(input("Enter the lane width adjustment (mph)"))
-frlc=float(input("Enter the right-side lateral clearane adjustment (mph)"))
+frlc=float(input("Enter the right-side lateral clearance adjustment (mph)"))
 TRD=float(input("Enter the total ramp density (ramps per mile)"))
 
 #calculate the free flow speed
